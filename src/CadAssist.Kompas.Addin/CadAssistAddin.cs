799420a79369b1a740756c8898c5dba381fe5edf
ArooiD <125826490+ArooiD@users.noreply.github.com>
2026-06-01 10:59:33 +0300

    Implement KOMPAS library COM registration

--- src/CadAssist.Kompas.Addin/CadAssistAddin.cs
+++ src/CadAssist.Kompas.Addin/CadAssistAddin.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
+using Microsoft.Win32;
 
 namespace CadAssist.Kompas.Addin
 {
@@ -10,9 +11,19 @@ namespace CadAssist.Kompas.Addin
     [ClassInterface(ClassInterfaceType.AutoDual)]
     public class CadAssistAddin
     {
-        public string Name
+        public string GetLibraryName()
         {
-            get { return "CAD Assist"; }
+            return "CAD Assist";
+        }
+
+        public short GetProtectNumber()
+        {
+            return 111;
+        }
+
+        public void ExternalRunCommand(short command, short mode, object kompas)
+        {
+            ShowTasks();
         }
 
         public void ShowTasks()
@@ -30,6 +41,49 @@ namespace CadAssist.Kompas.Addin
                 Arguments = "--model-path C:\\cad-assist-test\\test.m3d --show-task-window",
                 UseShellExecute = true
             });
+        }
+
+        [ComRegisterFunction]
+        public static void RegisterKompasLibrary(Type type)
+        {
+            var clsid = type.GUID.ToString("B").ToUpperInvariant();
+            var clsidKeyPath = @"SOFTWARE\Classes\CLSID\" + clsid;
+
+            using (var clsidKey = Registry.LocalMachine.CreateSubKey(clsidKeyPath))
+            {
+                if (clsidKey == null)
+                {
+                    throw new InvalidOperationException("Could not create CLSID registry key: " + clsidKeyPath);
+                }
+
+                clsidKey.SetValue(null, "CAD Assist KOMPAS Library");
+                clsidKey.CreateSubKey("Kompas_Library");
+
+                using (var inproc = clsidKey.CreateSubKey("InprocServer32"))
+                {
+                    if (inproc != null)
+                    {
+                        inproc.SetValue(null, Path.Combine(Environment.SystemDirectory, "mscoree.dll"));
+                        inproc.SetValue("ThreadingModel", "Both");
+                    }
+                }
+            }
+        }
+
+        [ComUnregisterFunction]
+        public static void UnregisterKompasLibrary(Type type)
+        {
+            var clsid = type.GUID.ToString("B").ToUpperInvariant();
+            var kompasLibraryKeyPath = @"SOFTWARE\Classes\CLSID\" + clsid + @"\Kompas_Library";
+
+            try
+            {
+                Registry.LocalMachine.DeleteSubKeyTree(kompasLibraryKeyPath, false);
+            }
+            catch
+            {
+                // Ignore unregister cleanup errors.
+            }
         }
     }
 }
