a93da13f09406e6f4715dfc41bfc7a4f58c4a6e6
ArooiD <125826490+ArooiD@users.noreply.github.com>
2026-06-01 10:40:52 +0300

    Use C# 7 compatible namespace syntax for addin

--- src/CadAssist.Kompas.Addin/CadAssistAddin.cs
+++ src/CadAssist.Kompas.Addin/CadAssistAddin.cs
@@ -3,29 +3,33 @@ using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 
-namespace CadAssist.Kompas.Addin;
-
-[ComVisible(true)]
-[Guid("E8D97B81-74F6-4D9E-B76C-47D4B687D9F7")]
-[ClassInterface(ClassInterfaceType.AutoDual)]
-public class CadAssistAddin
+namespace CadAssist.Kompas.Addin
 {
-    public string Name => "CAD Assist";
-
-    public void ShowTasks()
+    [ComVisible(true)]
+    [Guid("E8D97B81-74F6-4D9E-B76C-47D4B687D9F7")]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class CadAssistAddin
     {
-        var appExe = @"C:\cad-assist-test\app\CadAssist.Kompas.Interop.exe";
-
-        if (!File.Exists(appExe))
+        public string Name
         {
-            throw new FileNotFoundException("CAD Assist executable was not found", appExe);
+            get { return "CAD Assist"; }
         }
 
-        Process.Start(new ProcessStartInfo
+        public void ShowTasks()
         {
-            FileName = appExe,
-            Arguments = "--model-path C:\\cad-assist-test\\test.m3d --show-task-window",
-            UseShellExecute = true
-        });
+            var appExe = @"C:\cad-assist-test\app\CadAssist.Kompas.Interop.exe";
+
+            if (!File.Exists(appExe))
+            {
+                throw new FileNotFoundException("CAD Assist executable was not found", appExe);
+            }
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = appExe,
+                Arguments = "--model-path C:\\cad-assist-test\\test.m3d --show-task-window",
+                UseShellExecute = true
+            });
+        }
     }
 }
